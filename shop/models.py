e99b0370250e5747d6cabbf483dac7e6df62f2f2
cooklee <sbbogus@gmail.com>
2024-06-03 15:48:29 +0200

    added book model

--- shop/models.py
+++ shop/models.py
@@ -7,3 +7,13 @@ class Author(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    published_date = models.DateField()
+    isbn = models.CharField(max_length=13)
+
+    def __str__(self):
+        return f"{self.title} {self.author} {self.published_date} {self.isbn}"
