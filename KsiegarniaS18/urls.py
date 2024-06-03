70d83354a4db144f5e0e123884d301caa576791e
cooklee <sbbogus@gmail.com>
2024-06-03 11:55:04 +0200

    login view

--- KsiegarniaS18/urls.py
+++ KsiegarniaS18/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
+from accounts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", TemplateView.as_view(template_name="base.html"), name="base")
+    path("", TemplateView.as_view(template_name="base.html"), name="base"),
+    path('register/', views.CreateUserView.as_view(), name="register"),
+    path('login/', views.LoginView.as_view(), name="login"),
 ]
---
+++ accounts/views.py
@@ -0,0 +1,39 @@
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.views import View
+
+
+class CreateUserView(View):
+
+    def get(self, request):
+        return render(request, "accounts/create_user.html")
+
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+        if password != "" and password == password2:
+            u = User(username=username)
+            u.set_password(password)
+            u.save()
+            return redirect('base')
+        return render(request, "accounts/create_user.html", {"error": "Passwords do not match"})
+
+# Create your views here.
+class LoginView(View):
+
+
+    def get(self, request):
+        return render(request, "accounts/login.html")
+
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('base')
+        else:
+            return redirect('login')
